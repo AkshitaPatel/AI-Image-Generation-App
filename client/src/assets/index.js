8ae7e5e6846848b0c778f3894070e2f8a8cf6b98
AkshitaPatel <akshitapatel02@gmail.com>
2023-02-06 12:22:07 -0500

    Integrating client server and adding generate and search functionalities

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,6 @@
 // import download from "./download.png";
 import logo from "./logo.svg";
 import preview from "./preview.png";
+import download from "./download.png";
 
-export { logo, preview };
+export { logo, preview, download };
